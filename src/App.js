bef9792f120f7b5d0777954486128b36e6878165
partabot <pierre@partabot.com>
2025-09-01 20:30:21 -0700

    add all

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="company-name"><u>Safe Sentinel Inc.</u></h1>
+      <p className="description">
+        We are a small group of machine learning engineers and robotics engineers who previously worked at Tesla and contributed to leading OSS robotics projects.
+        <br /><br />
+        We are currently building robots for industries and are partnering with a national retailer as our first design partner.
+        <br /><br />
+        Our eventual goal is to build general purpose robots that can self-replicate.
+        <br /><br />
+        If you would like to reach out, please email us at <strong>hello@safesentinels.com</strong>
+      </p>
+    </div>
+  );
+}
+
+export default App;
